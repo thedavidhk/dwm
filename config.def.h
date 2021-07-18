c600ee28afb6e3d7bee67f0c6e8529da7aa759ac
TheDavidHk <herok.davidj@gmail.com>
2021-07-18 18:37:56 +0200

    Added gaps, hide vacant tags, changed colors

--- config.def.h
+++ config.def.h
@@ -1,17 +1,20 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "DejaVu Sans Mono:size=11" };
+static const char dmenufont[]       = "DejaVu Sans Mono:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#334455";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -59,11 +62,21 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *volume_up[]  = { "amixer", "set", "Master", "5%+", NULL };
+static const char *volume_down[]  = { "amixer", "set", "Master", "5%-", NULL };
+static const char *volume_mute[]  = { "amixer", "set", "Master", "toggle", NULL };
+static const char *brightness_up[]  = { "xbacklight", "inc", "5", NULL };
+static const char *brightness_down[]  = { "xbacklight", "dec", "5", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,		XF86XK_AudioRaiseVolume,   spawn,          {.v = volume_up } },
+	{ 0,		XF86XK_AudioLowerVolume,   spawn,          {.v = volume_down } },
+	{ 0,		XF86XK_AudioMute,	   spawn,          {.v = volume_mute } },
+	{ 0,		XF86XK_MonBrightnessUp,	   spawn,          {.v = brightness_up } },
+	{ 0,		XF86XK_MonBrightnessDown,  spawn,          {.v = brightness_down } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
